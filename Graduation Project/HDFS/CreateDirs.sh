225fa6501d21a4282747d24d7548f30f093fd4cc
AhmedMetwaly1287 <ahmedmetwaly1287@gmail.com>
2024-03-22 16:45:35 +0200

    Create CreateDirs.sh

---
+++ Graduation Project/HDFS/CreateDirs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Your bash code here
+mkdir /home/cloudera/covid_project
+echo "Main Project dir Created Successfully"
+
+mkdir /home/cloudera/covid_project/landing_zone
+echo "Landing_zone Sub dir Created Successfully"
+
+mkdir /home/cloudera/covid_project/landing_zone/COVID_SRC_LZ
+echo "Dataset dir Created Successfully"
+
+mkdir /home/cloudera/HQL
+echo "HQL Scripts dir Created Successfully"
+
+mkdir /home/cloudera/script
+echo "Shell Script dir Created Successfully"
+
+echo "All Dirs Created Successfully"
